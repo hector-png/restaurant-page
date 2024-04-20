478828c32a744b86e1b7e2e0d7f6f27ad56fef41
Hector Atilano <159952379+daniel-Hector@users.noreply.github.com>
2024-04-19 22:07:21 -0600

    Add logic for tabs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-import { initialPageLoad } from './page-load';
+import { initialPageLoad } from './home';
+import { menuLoad } from './menu';
+import { aboutLoad } from './about';
 import './style.css';
 
 initialPageLoad();
+
+(function tabSwitch() {
+    const navButtons = document.querySelectorAll('nav > button');
+
+    const handleClick = (e) => {
+        const buttonClicked = e.target.id;
+        switch(buttonClicked) {
+            case 'menu': menuLoad();
+            break;
+            case 'about': aboutLoad();
+            break;
+            case 'home': initialPageLoad();
+            break;
+        };
+    };
+    
+    const init = () => navButtons.forEach(button => {
+        button.addEventListener('click', handleClick);
+    });
+
+    init();
+})();
